c95d2ca5ed324c22d708dc131783d4bc250a46f2
Millena Bomtempo <millena.bomtempo@gmail.com>
2025-07-13 11:56:30 -0300

    fix: add suspense

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
+
 import { CartWidget } from "./cart-widget";
 import { SearchForm } from "./search-form";
 
@@ -11,13 +13,15 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
         <CartWidget />
 
-        <div className="w-px h-4 bg-zinc-700" />
+        <div className="w-px h-4 bg-zinc-700"></div>
 
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
